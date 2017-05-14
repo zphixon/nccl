5007731c141325e606241e770d6aebc1d25cb5b2
cheezgi <zackh@firemail.cc>
2017-05-13 22:01:13 -0400

    impl Error for Error

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,27 @@
 
 #[derive(Debug)]
+/// nccl Error type.
 pub enum Error {
     KeyNotFound,
     IndentationError,
     NameError,
+}
+
+impl ::std::fmt::Display for Error {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        write!(f, "An error has ocurred: {:?}", self)
+    }
+}
+
+#[allow(unreachable_patterns)]
+impl ::std::error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::KeyNotFound => "Key not found.",
+            Error::IndentationError => "Incorrect indentation: Not 4 spaces.",
+            Error::NameError => "Schema not found.",
+            _ => "Unknown error."
+        }
+    }
 }
 
