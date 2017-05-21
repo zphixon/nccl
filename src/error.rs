fa8947e116315aa45ca53c97a27a27bb28fea2b8
cheezgi <zackh@firemail.cc>
2017-05-21 12:25:07 -0400

    clean up

--- src/error.rs
+++ src/error.rs
@@ -21,11 +21,13 @@ pub struct NcclError {
 
 impl NcclError {
     pub fn new(kind: ErrorKind, message: &str, line: u64) -> Self {
-        NcclError {
+        let r = NcclError {
             kind: kind,
             message: message.to_owned(),
             line: line,
-        }
+        };
+        println!("{}", &r);
+        r
     }
 }
 
@@ -34,23 +36,4 @@ impl fmt::Display for NcclError {
         write!(f, "An error has ocurred: {:?} on line {}\n\t{}", self.kind, self.line, self.message)
     }
 }
-
-//impl ::std::fmt::Display for ErrorKind {
-//    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-//        write!(f, "An error has ocurred: {:?}", self)
-//    }
-//}
-//
-//impl ::std::error::Error for Error {
-//    fn description(&self) -> &str {
-//        match *self {
-//            Error::KeyNotFound => "Key not found.",
-//            Error::IndentationError => "Incorrect indentation: Not 4 spaces.",
-//            Error::NameError => "Schema not found.",
-//            Error::NoValue => "No value associated with key.",
-//            Error::ParseError => "Unable to parse value.",
-//            Error::FileError => "Could not open file.",
-//        }
-//    }
-//}
 
