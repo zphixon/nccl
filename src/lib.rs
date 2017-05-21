15db53d96114bd164218cdd87e1cc5f4d676e4a4
cheezgi <zackh@firemail.cc>
2017-05-21 17:20:07 -0400

    start parse_file

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,17 @@ pub use value::*;
 pub use token::*;
 pub use scanner::*;
 
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
+pub fn parse_file(filename: &str) -> Result<Vec<Token>, NcclError> {
+    if let Ok(mut file) = File::open(Path::new(filename)) {
+        let mut data = String::new();
+        file.read_to_string(&mut data).unwrap();
+        Scanner::new(data).scan_tokens()
+    } else {
+        Err(NcclError::new(ErrorKind::FileError, "Could not find file.", 0))
+    }
+}
+
