afadd4cfb7ae141f913ede7447bc008b541f2fc7
cheezgi <zackh@firemail.cc>
2017-06-18 21:33:59 -0400

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,26 @@ use std::path::Path;
 use std::fs::File;
 use std::io::Read;
 
-pub fn parse_file(filename: &str) -> Result<Vec<Token>, Vec<NcclError>> {
+pub fn parse_file(filename: &str) -> Result<Pair, Vec<NcclError>> {
     if let Ok(mut file) = File::open(Path::new(filename)) {
         let mut data = String::new();
         file.read_to_string(&mut data).unwrap();
-        Scanner::new(data).scan_tokens()
+        //let tokens = Scanner::new(data).scan_tokens()?;
+        Parser::new(Scanner::new(data).scan_tokens()?).parse()
     } else {
         Err(vec![NcclError::new(ErrorKind::FileError, "Could not find file.", 0)])
+    }
+}
+
+pub fn print_tokens(tokens: Vec<Token>) {
+    for token in tokens {
+        match token.kind {
+            TokenKind::Value => print!("\"{}\"", token.lexeme),
+            TokenKind::Colon => print!(":"),
+            TokenKind::Indent => print!("    "),
+            TokenKind::Newline => println!(""),
+            TokenKind::EOF => println!("end"),
+        }
     }
 }
 
