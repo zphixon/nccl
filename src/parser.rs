ec8bf2477a0fb8de8c6c1f2a7510f98614cf4bbd
cheezgi <zackh@firemail.cc>
2017-06-24 17:34:48 -0400

    error if incorrect indentation

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 
 use pair::Pair;
 use token::{Token, TokenKind};
+use error::{NcclError, ErrorKind};
 
 use std::error::Error;
 
@@ -40,6 +41,8 @@ impl Parser {
     // faked you out with that Scanner, didn't I?
     // you thought this was going to be recursive descent. YOU WERE WRONG!
     pub fn parse(mut self) -> Result<Pair, Vec<Box<Error>>> {
+        let mut errors: Vec<Box<Error>> = vec![];
+        let mut prev_indent = 0;
         let mut i = 0;
 
         while i < self.tokens.len() {
@@ -71,10 +74,25 @@ impl Parser {
 
                     i -= 1;
 
-                    self.indent = indent;
+                    if prev_indent > indent {
+                        if prev_indent - indent == 1 || prev_indent - indent == 0 {
+                            self.indent = indent;
+                        } else {
+                            errors.push(Box::new(NcclError::new(ErrorKind::IndentationError, "Incorrect level of indentation found", self.line)));
+                            self.indent = prev_indent;
+                        }
+                    } else {
+                        if indent - prev_indent == 1 || indent - prev_indent == 0 {
+                            self.indent = indent;
+                        } else {
+                            errors.push(Box::new(NcclError::new(ErrorKind::IndentationError, "Incorrect level of indentation found", self.line)));
+                            self.indent = prev_indent;
+                        }
+                    }
                 },
 
                 TokenKind::Newline => { // reset self.index
+                    prev_indent = self.indent;
                     self.indent = 0;
                     self.line += 1;
                 },
@@ -84,7 +102,11 @@ impl Parser {
             i += 1;
         }
 
-        Ok(self.pair)
+        if errors.len() == 0 {
+            Ok(self.pair)
+        } else {
+            Err(errors)
+        }
     }
 }
 
